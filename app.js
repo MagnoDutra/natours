82583f0b3910af45f2f2b0058494aee6a0385b10
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-08 20:35:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Magnao' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  // chamado assim que o server starta
+  console.log(`App running on port ${port}...`);
+});
