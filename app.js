64b8c3434096344d4504adcced02de00c4be5959
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-09 00:13:17 -0300

    criado routers

--- app.js
+++ app.js
@@ -140,19 +140,19 @@ function deleteUser(req, res) {
 }
 
 // 3) Routes ---------------------------------------------------
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app.route('api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 // 4) Start server ---------------------------------------------------
 
 const port = 3000;
