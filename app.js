d507a55ed6ec64490832daf9f1b4b5c43856d7f3
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-08 21:23:51 -0300

    respondendo a parametros na url

--- app.js
+++ app.js
@@ -27,6 +27,25 @@ app.get('/api/v1/tours', (req, res)=>{
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res)=>{
+  const id = req.params.id * 1; // Converto para numero pois ele vem como string
+  const tour = tours.find(el => el.id === id);
+
+  if(!tour) {
+    return res.status(404).send({
+      status: "failed",
+      message: "Invalid ID"
+    });
+  }
+  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req,res)=>{
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newId}, req.body);
