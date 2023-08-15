65107585d1bc07fa60e7fbb86c1af730b8c3a6cc
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-15 01:08:30 -0300

    testando o mongoose adicionando dados

--- server.js
+++ server.js
@@ -35,6 +35,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.5,
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error!!:', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // chamado assim que o server starta
